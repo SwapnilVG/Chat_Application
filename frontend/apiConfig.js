767b6da392a1d2f041dbf5c769b48feb2698b03e
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-08-31 18:28:24 +0530

    Assignment Completed

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -2,20 +2,13 @@
 let baseUrl;
 let socketUrl;
 
-// if (import.meta.env.VITE_NODE_ENV === "production") {
-//   baseUrl = "https://simple-chat-app-6yeb.onrender.com";
-//   socketUrl = "wss://simple-chat-app-6yeb.onrender.com";
-// } else {
-//   baseUrl = "http://localhost:4000";
-//   socketUrl = "ws://localhost:4000";
-// }
-
 if (import.meta.env.VITE_NODE_ENV === "production") {
-  baseUrl = "http://localhost:4000";
-  socketUrl = "http://localhost:4000";
+  baseUrl = "https://chat-application-backend-dun.vercel.app/";
+  socketUrl = "https://chat-application-backend-dun.vercel.app/";
 } else {
   baseUrl = "http://localhost:4000";
   socketUrl = "ws://localhost:4000";
 }
+
 
 export { baseUrl, socketUrl };
