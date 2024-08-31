ce4ca1f3eb2d6a689e528ff403a545cf518b00e8
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-08-31 23:49:09 +0530

    Assignment Completed

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -1,5 +1,5 @@
 // apiConfig.js
 let  baseUrl = "https://chat-application-backend-dun.vercel.app/";
-let  socketUrl = "wss://chat-application-backend-dun.vercel.app/";
+let  socketUrl = "https://chat-application-backend-dun.vercel.app/";
 
 export { baseUrl, socketUrl };
