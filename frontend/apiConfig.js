4065014954bd6aa24492170d3b581989b5a26a9c
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-08-31 23:31:39 +0530

    Assignment Completed

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -1,14 +1,14 @@
 // apiConfig.js
-let baseUrl;
-let socketUrl;
+let  baseUrl = "https://chat-application-backend-dun.vercel.app/";
+let  socketUrl = "https://chat-application-backend-dun.vercel.app/";
 
-if (import.meta.env.VITE_NODE_ENV === "production") {
-  baseUrl = "https://chat-application-backend-dun.vercel.app/";
-  socketUrl = "https://chat-application-backend-dun.vercel.app/";
-} else {
-  baseUrl = "http://localhost:4000";
-  socketUrl = "ws://localhost:4000";
-}
+// if (import.meta.env.VITE_NODE_ENV === "production") {
+//   baseUrl = "https://chat-application-backend-dun.vercel.app/";
+//   socketUrl = "https://chat-application-backend-dun.vercel.app/";
+// } else {
+//   baseUrl = "http://localhost:4000";
+//   socketUrl = "ws://localhost:4000";
+// }
 
 
 export { baseUrl, socketUrl };
