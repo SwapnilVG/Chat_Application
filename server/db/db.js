b8e225e8debce3f35d402ed7c4d282745f74c118
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-08-31 22:33:09 +0530

    Assignment Completed

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,21 @@
-import mongoose from 'mongoose';
+
+import mongoose from "mongoose";
 
 // Function to connect to the database
-export default async function connectDB() {
-  try {
-    await mongoose.connect(process.env.DB);
-    console.log("DB CONNECTED SUCCESSFULLY");
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-    console.log("COULD NOT CONNECT TO DB");
-  }
+mongoose.set('strictQuery',false)
+const connectDB = async () =>{
+    try {
+        const {connection} = await mongoose.connect(process.env.DB || "mongodb://127.0.0.1:27017/Chat_Application")
+        if(connection){
+            console.log(`Connected to MongoDB: ${connection.host}`)
+        }
+    } catch (error) {
+        console.log("Fail to Connect Database",error)
+        console.log("COULD NOT CONNECT TO DB");
+        process.exit(1)
+    }
 }
+
+
+export default connectDB;
+
